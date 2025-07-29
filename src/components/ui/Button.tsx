ed6e4fd54762e20e146125a0696d2bb6971735b6
김진 <miyukikj35@gamil.com>
2025-07-29 15:53:33 +0900

    Chore: variant 오타 수정

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,15 +3,15 @@ import { ButtonHTMLAttributes, ReactNode } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  varient?: "filled" | "outline";
+  variant?: "filled" | "outline";
 }
 
-const Button = ({ children, varient = "filled", ...rest }: ButtonProps) => {
+const Button = ({ children, variant = "filled", ...rest }: ButtonProps) => {
   const baseClass =
     "w-full text-xl p-4 rounded-[10px] cursor-pointer";
 
   const variantClass =
-    varient === "filled"
+    variant === "filled"
       ? "bg-[#405DE6] text-white"
       : "border border-[#405DE6] text-[#405DE6] bg-white";
 
