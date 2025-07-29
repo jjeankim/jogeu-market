923c1f4fe3e50e0f397c7eaafe51edb4f36fad56
Youngsun33 <heoyoungsun33@gmail.com>
2025-07-29 15:10:56 +0900

    Fix : 메인 홈페이지 이미지와 링크 수정. 백그라운드 컬러 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <header className="m-10 flex justify-center content-center flex-col">
-      <img src="/images/logo_jogeuMarket.svg" alt="로고" className="w-40 mx-auto" />
+    <header className="m-10 flex justify-center items-center flex-col">
+      <div className="flex justify-center items-center">
+        <Image width={150} height={1} src="/images/logo_jogeuMarket.svg" alt="로고"/>
+      </div>
       <nav className="text-center m-5">
-        <a href="/"> Best |</a>
-        <a href="/"> Brands |</a>
-        <a href="/"> Products |</a>
-        <a href="/"> Review</a>
+        <Link href="/"> Best |</Link>
+        <Link href="/"> Brands |</Link>
+        <Link href="/"> Products |</Link>
+        <Link href="/"> Review</Link>
       </nav>
     </header>
   );
