fa1d97364c67bd7d5714b373aa704bfc31106d55
Youngsun33 <heoyoungsun33@gmail.com>
2025-07-30 12:09:20 +0900

    Fix : 헤더 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@
 import DynamicBreadcrumb from "@/components/ui/autoBreadcrumb";
 import Footer from "@/components/ui/Footer";
 import Main from "./main";
+import Header from "@/components/Header";
         
 export default function Home() {
   return (
     <>
+      <Header />
        <Main />
       <DynamicBreadcrumb />
       <Footer />
