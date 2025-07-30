05de4c974297a9dccfcf2516eb788e9297568d22
dev-suz <return@knou.ac.kr>
2025-07-30 11:21:04 +0900

    AuthStore AccessToken 로컬스토리지 넣는 로직 추가, AuthLink description추가, LoginForm CustomInprregister 함수 주석처리 - 이펙트 모니터링에 영향준다고합니다

---
+++ src/components/ui/MyPageAside.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MyPageAside = () => {
+  return (
+    <>
+      <aside className="w-1/4 bg-amber-400 ml-28 mt-30  h-2/3">
+        <ul>
+          <li>주문내역</li>
+          <li>찜한 상품</li>
+          <li>리뷰 작성</li>
+          <li>개인 정보</li>
+          <li>내 쿠폰함</li>
+          <li>문의 사항</li>
+        </ul>
+      </aside>
+    </>
+  );
+};
+
+export default MyPageAside;
