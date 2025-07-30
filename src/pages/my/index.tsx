15e733a96afe19170569591c73af1d80452528b4
김진 <miyukikj35@gamil.com>
2025-07-30 16:37:54 +0900

    Chore:중복 select 삭제

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -6,19 +6,17 @@ import Image from "next/image";
 const index = () => {
   return (
     <MyPageLayout pageTitle="마이페이지">
-      <section>
-        <MyPageInfo />
-        <MyPageOrderStatusCard />
-        {/* 오더 없을때 조건 부 렌더링 */}
-        <div className="flex justify-center">
-          <Image
-            width={400}
-            height={400}
-            src={"/images/logo_bw_jogeuMarket.svg"}
-            alt="진행 중인 주문 없음 이미지"
-          />
-        </div>
-      </section>
+      <MyPageInfo />
+      <MyPageOrderStatusCard />
+      {/* 오더 없을때 조건 부 렌더링 */}
+      <div className="flex justify-center">
+        <Image
+          width={400}
+          height={400}
+          src={"/images/logo_bw_jogeuMarket.svg"}
+          alt="진행 중인 주문 없음 이미지"
+        />
+      </div>
     </MyPageLayout>
   );
 };
