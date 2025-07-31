05de79ceaa253a0365875e8b22140cac32f8d642
김진 <miyukikj35@gamil.com>
2025-07-31 16:27:49 +0900

    Feat: 마이페이지 내 정보 수정 작업중

---
+++ src/components/my/MypageUserSetting.tsx
@@ -0,0 +1,14 @@
+import { SubTitle } from "@/pages/my/coupons";
+
+const MypageUserSetting = () => {
+  return (
+    <div>
+      <SubTitle title="회원 정보 관리" />
+      <div className="border-2 rounded-2xl px-8 py-2">
+
+      </div> 
+    </div>
+  ); 
+};
+
+export default MypageUserSetting;
---
+++ src/pages/my/settings/index.tsx
@@ -0,0 +1,13 @@
+import MyPageLayoutWithWelcome from "@/components/my/MyPageLayoutWithWelcome"
+import MypageUserSetting from "@/components/my/MypageUserSetting"
+
+
+const index = () => {
+  return (
+    <MyPageLayoutWithWelcome>
+      <MypageUserSetting/>
+    </MyPageLayoutWithWelcome>
+  )
+}
+
+export default index
