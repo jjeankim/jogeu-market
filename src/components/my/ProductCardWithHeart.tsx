b81378d48bade2724116db3cf410f82ddf65726b
dev-suz <return@knou.ac.kr>
2025-08-01 09:49:02 +0900

    하트 들어간 상품 카드 및 찜 목록 페이지

---
+++ src/components/my/ProductCardWithHeart.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import ProductCard from "@/components/ui/ProductCard";
+import { FiHeart } from "react-icons/fi";
+import { FaHeart } from "react-icons/fa";
+
+type Product = {
+  name: string;
+  price: number;
+  imgUrl?: string;
+  shippingFee?: number;
+};
+
+interface ProductCardWithHeartProps {
+  product: Product;
+  initiallyLiked?: boolean;
+  showShippingInfo?: boolean;
+  onLikeToggle?: (liked: boolean) => void;
+}
+
+const ProductCardWithHeart = ({
+  product,
+  initiallyLiked = true,
+  showShippingInfo = true,
+  onLikeToggle,
+}: ProductCardWithHeartProps) => {
+  const [liked, setLiked] = useState(initiallyLiked);
+
+  const toggleLike = () => {
+    const newState = !liked;
+    setLiked(newState);
+    onLikeToggle?.(newState); // 콜백으로 외부에 상태 전달 (옵션)
+  };
+
+  return (
+    <div className="relative inline-block max-w-[1200px] mx-auto">
+      <ProductCard product={product} showShippingInfo={showShippingInfo} />
+
+      <span
+        className="absolute bottom-10 right-10 cursor-pointer z-10"
+        onClick={toggleLike}
+      >
+        {liked ? (
+          <FaHeart size={32} className="text-red-600" />
+        ) : (
+          <FiHeart size={32} className="text-black" />
+        )}
+      </span>
+    </div>
+  );
+};
+
+export default ProductCardWithHeart;
