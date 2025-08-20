e804bb249514c8ee4faaa1c7daa1891829134397
dev-suz <return@knou.ac.kr>
2025-08-21 06:54:10 +0900

    fix:이미지 관련 레이아웃 위험요소 수정

--- src/components/ui/AuthFormLayout.tsx
+++ src/components/ui/AuthFormLayout.tsx
@@ -13,16 +13,16 @@ const AuthFormLayout = ({
     <div className="flex items-center justify-center min-h-screen m-8">
       <div className="w-full max-w-xl">
         <div className="flex justify-center mb-24">
-
-        <Link href={"/"} >
-          <Image
-            width={180}
-            height={180}
-            src={"/images/logo_jogeuMarket.svg"}
-            alt="조그마켓 로고"
+          <Link href={"/"}>
+            <Image
+              width={180}
+              height={0}
+              style={{ height: "auto" }}
+              src={"/images/logo_jogeuMarket.svg"}
+              alt="조그마켓 로고"
             />
-        </Link>
-            </div>
+          </Link>
+        </div>
         <h2 className="text-3xl font-bold text-center mb-10">{title}</h2>
         {children}
       </div>
